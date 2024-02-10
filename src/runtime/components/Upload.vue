6db2667bb52f6de167264db8d60cf12b6a66d3a8
Hironori Kikuchi <kikuchi@xenodrive.co.jp>
2024-02-11 00:05:28 +0900

    Use Button component for Upload by default

--- src/runtime/components/Upload.vue
+++ src/runtime/components/Upload.vue
@@ -1,11 +1,13 @@
 <template>
-  <Component :is="props.tag ?? 'button'" @click="click()">
+  <Component :is="props.tag ?? Button" @click="click()">
     <slot />
     <input ref="input" type="file" style="display: none" accept="application/json" @change="onchange($event)" />
   </Component>
 </template>
 
 <script lang="ts" setup>
+import Button from './Button.vue';
+
 import { ref } from '#imports';
 type Props = {
   tag?: string;
