79cc906491adab14ad9c88f24f55b1802386867a
Hironori Kikuchi <kikuchi@xenodrive.co.jp>
2024-04-19 11:12:28 +0900

    Fix tailwind.config

---
+++ playground/pages/dialog.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-indigo-100 p-2">
+    <div class="">
+      <Dialog v-model="model">
+        <template #trigger>Open</template>
+        <div>dialog</div>
+      </Dialog>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const model = ref({});
+</script>
--- src/runtime/tailwind.config.ts
+++ src/runtime/tailwind.config.ts
@@ -10,100 +10,104 @@ interface CustomThemeConfig extends ThemeConfig {
 }
 
 export default {
-  theme: {
-    extend: {
-      colors: ({ colors }: any) => ({
-        primary: colors.indigo['500'],
-        invert: colors.white,
-      }),
+  presets: [
+    {
+      theme: {
+        extend: {
+          colors: ({ colors }: any) => ({
+            primary: colors.indigo['500'],
+            invert: colors.white,
+          }),
+        },
+
+        checkbox: ({ theme }) => ({
+          active: {
+            icon: theme('colors.primary'),
+            background: 'transparent',
+          },
+          disabled: {
+            icon: 'inherit',
+            background: 'transparent',
+          },
+          hover: {
+            icon: theme('colors.primary'),
+            background: theme('colors.primary'),
+            backgroundOpacity: '25%',
+          },
+        }),
+
+        toggle: ({ theme }) => ({
+          height: '1.25rem',
+          width: '2.5rem',
+
+          knob: {
+            size: '1rem',
+            off: theme('colors.white'),
+            on: theme('colors.white'),
+          },
+
+          background: {
+            off: theme('colors.gray.200'),
+            on: theme('colors.primary'),
+          },
+
+          ring: {
+            size: '2px',
+            color: theme('colors.primary'),
+            opacity: '30%',
+          },
+        }),
+
+        dropdown: ({ theme }) => ({
+          selected: {
+            text: 'inherit',
+            background: theme('colors.slate.200'),
+            backgroundOpacity: '100%',
+          },
+          hover: {
+            text: 'inherit',
+            background: theme('colors.slate.200'),
+            backgroundOpacity: '100%',
+          },
+        }),
+
+        tag: ({ theme }) => ({
+          dark: {
+            text: theme('colors.slate.100'),
+          },
+          light: {
+            text: theme('colors.slate.900'),
+          },
+          background: theme('colors.slate.200'),
+          text: theme('colors.slate.900'),
+        }),
+
+        badge: ({ theme }) => ({
+          dark: {
+            text: theme('colors.slate.100'),
+          },
+          light: {
+            text: theme('colors.slate.900'),
+          },
+          title: {
+            background: theme('colors.slate.800'),
+            text: theme('colors.slate.100'),
+          },
+          value: {
+            background: theme('colors.slate.200'),
+            text: theme('colors.slate.900'),
+          },
+        }),
+
+        dialog: {
+          commit: {
+            label: 'OK',
+          },
+          cancel: {
+            label: 'Cancel',
+          },
+        },
+      } satisfies Partial<CustomThemeConfig & { extend: Partial<CustomThemeConfig> }>,
     },
-
-    checkbox: ({ theme }) => ({
-      active: {
-        icon: theme('colors.primary'),
-        background: 'transparent',
-      },
-      disabled: {
-        icon: 'inherit',
-        background: 'transparent',
-      },
-      hover: {
-        icon: theme('colors.primary'),
-        background: theme('colors.primary'),
-        backgroundOpacity: '25%',
-      },
-    }),
-
-    toggle: ({ theme }) => ({
-      height: '1.25rem',
-      width: '2.5rem',
-
-      knob: {
-        size: '1rem',
-        off: theme('colors.white'),
-        on: theme('colors.white'),
-      },
-
-      background: {
-        off: theme('colors.gray.200'),
-        on: theme('colors.primary'),
-      },
-
-      ring: {
-        size: '2px',
-        color: theme('colors.primary'),
-        opacity: '30%',
-      },
-    }),
-
-    dropdown: ({ theme }) => ({
-      selected: {
-        text: 'inherit',
-        background: theme('colors.slate.200'),
-        backgroundOpacity: '100%',
-      },
-      hover: {
-        text: 'inherit',
-        background: theme('colors.slate.200'),
-        backgroundOpacity: '100%',
-      },
-    }),
-
-    tag: ({ theme }) => ({
-      dark: {
-        text: theme('colors.slate.100'),
-      },
-      light: {
-        text: theme('colors.slate.900'),
-      },
-      background: theme('colors.slate.200'),
-      text: theme('colors.slate.900'),
-    }),
-
-    badge: ({ theme }) => ({
-      dark: {
-        text: theme('colors.slate.100'),
-      },
-      light: {
-        text: theme('colors.slate.900'),
-      },
-      title: {
-        background: theme('colors.slate.800'),
-        text: theme('colors.slate.100'),
-      },
-      value: {
-        background: theme('colors.slate.200'),
-        text: theme('colors.slate.900'),
-      },
-    }),
-
-    dialog: {
-      commit: {
-        label: 'OK',
-      },
-      cancel: {
-        label: 'Cancel',
-      },
-    },
-  } satisfies Partial<CustomThemeConfig & { extend: Partial<CustomThemeConfig> }>,
+  ],
 };
