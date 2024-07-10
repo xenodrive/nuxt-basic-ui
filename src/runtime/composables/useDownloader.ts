282b788851dd3cc71ad75365ff35a62fb295db0f
Hironori Kikuchi <kikuchi@xenodrive.co.jp>
2024-07-10 10:46:32 +0900

    Add `downloading` to useDownloader

--- src/runtime/composables/useDownloader.ts
+++ src/runtime/composables/useDownloader.ts
@@ -1,35 +1,45 @@
+import { ref } from '#imports';
+
 function basename(path: string) {
   return path.split('/').reverse()[0];
 }
 
-async function download(url: string, fetchOption: RequestInit = {}, filename?: string) {
-  const res = await fetch(url, fetchOption);
-
-  const contentDisposition = res.headers
-    .get('Content-Disposition')
-    ?.replace(/.*filename\*=UTF-8''(.*)/, (...match) => decodeURI(match[1]));
-
-  return downloadBlobAs(filename || contentDisposition || basename(url), await res.blob());
-}
-
-function downloadBlobAs(filename: string, blob: Blob) {
-  const obj = window.URL.createObjectURL(blob);
-
-  const downloader = document.createElement('a');
-  downloader.setAttribute('href', obj);
-  downloader.setAttribute('download', filename);
-  downloader.click();
-
-  setTimeout(() => {
-    window.URL.revokeObjectURL(obj);
-  }, 100);
-
-  return true;
-}
-
 export default function () {
+  const downloading = ref(false);
+
+  async function download(url: string, fetchOption: RequestInit = {}, filename?: string) {
+    downloading.value = true;
+    try {
+      const res = await fetch(url, fetchOption);
+
+      const contentDisposition = res.headers
+        .get('Content-Disposition')
+        ?.replace(/.*filename\*=UTF-8''(.*)/, (...match) => decodeURI(match[1]));
+
+      return downloadBlobAs(filename || contentDisposition || basename(url), await res.blob());
+    } finally {
+      downloading.value = false;
+    }
+  }
+
+  function downloadBlobAs(filename: string, blob: Blob) {
+    const obj = window.URL.createObjectURL(blob);
+
+    const downloader = document.createElement('a');
+    downloader.setAttribute('href', obj);
+    downloader.setAttribute('download', filename);
+    downloader.click();
+
+    setTimeout(() => {
+      window.URL.revokeObjectURL(obj);
+    }, 100);
+
+    return true;
+  }
+
   return {
     download,
     downloadBlobAs,
+    downloading,
   };
 }
