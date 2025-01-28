1c5083b87af642d0cf6f5602763ddc36a4da4faa
Hironori KIKUCHI <kikuchi@xenodrive.co.jp>
2025-01-28 14:42:33 +0900

    update: Adjust pages

--- playground/pages/page.vue
+++ playground/pages/page.vue
@@ -1,20 +1,21 @@
 <template>
-  <Page class="h-screen!">
-    <h1>The page</h1>
-
+  <Page>
     <template #header>
       <div class="bg-slate-300">
         <div>header1</div>
         <div>header2</div>
       </div>
     </template>
+
+    <h1>The page</h1>
+
     body
 
-    <div class="w-[200px] border">full w</div>
-    <div class="h-[200px] border">full h</div>
+    <div class="w-[1000px] border">w</div>
+    <div class="inline-block h-[1000px] border">h</div>
 
     <template #footer>
-      <div class="flex gap-4 bg-slate-300">
+      <div class="flex justify-between bg-slate-300">
         <div>footer1</div>
         <div>footer2</div>
       </div>
--- src/runtime/components/Page.vue
+++ src/runtime/components/Page.vue
@@ -2,11 +2,16 @@
   <div class="page">
     <Loading :model-value="props.loading" />
     <header><slot name="header" /></header>
-    <div class="page-container">
-      <div class="page-scroller">
-        <div class="page-contents">
-          <slot />
-        </div>
+    <div class="page-viewport">
+      <div
+        class="page-contents"
+        :class="props.contentClass"
+        :style="{
+          height: props.hFull ? '100%' : undefined,
+          width: props.wFull ? '100%' : undefined,
+          padding: props.padding ?? undefined,
+        }">
+        <slot />
       </div>
     </div>
     <footer><slot name="footer" /></footer>
@@ -14,38 +19,47 @@
 </template>
 
 <script lang="ts" setup>
+import type { ClassNameValue } from 'tailwind-merge';
+
 type Props = {
   loading?: boolean;
+  contentClass?: ClassNameValue;
+  wFull?: boolean;
+  hFull?: boolean;
+  padding?: string | number;
 };
 const props = defineProps<Props>();
 </script>
 
+<style>
+html,
+body,
+#__nuxt {
+  width: 100%;
+  height: 100%;
+}
+</style>
+
 <style scoped>
-@import '../assets/css/theme.css' theme(reference);
+@import '../assets/css/theme.css';
 
 .page {
   @apply relative flex h-full w-full flex-col;
 
-  > .page-container {
-    @apply h-full overflow-scroll;
-
-    > .page-scroller {
-      @apply mx-auto h-fit w-fit p-4;
-
-      > .page-contents {
-        @apply h-fit w-fit min-w-(--breakpoint-xl);
-
-        &:deep(h1) {
-          display: flex;
-
-          @apply flex items-center gap-4 text-lg font-bold;
-        }
-      }
-    }
-  }
-
   > header {
     @apply w-full flex-none;
+  }
+
+  > .page-viewport {
+    @apply relative w-full flex-grow basis-full overflow-x-auto overflow-y-scroll;
+
+    > .page-contents {
+      @apply mx-auto h-fit w-fit min-w-(--breakpoint-xl) p-4;
+
+      &:deep(h1) {
+        @apply flex items-center gap-4 text-lg font-bold;
+      }
+    }
   }
 
   > footer {
